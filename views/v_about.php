447eb99a345551e495709ad9f89d14731fbadbaf
Muhammadrafi295 <rafimuhammad295@gmail.com>
2023-04-02 22:01:05 +0700

    latihan1

---
+++ views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Muhammad</li>
+    <li>Nama Belakang : Rafi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Kp. Pasir Putih, Sawangan, Kota Depok</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Depok</li>
+ </ul>
+ 
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+    <li>Badminton</li>
+    <li>Mancing</li>
+ </ul>
+</section>
